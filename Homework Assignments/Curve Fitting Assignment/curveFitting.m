9ce97c1164f73b3086ff8cd931b8b4e27c03c714
avl39 <avl39@pitt.edu>
2024-03-21 15:16:09 -0400

    Added finished curveFitting assignment and dataset

--- Homework Assignments/Curve Fitting Assignment/curveFitting.m
+++ Homework Assignments/Curve Fitting Assignment/curveFitting.m
@@ -1,21 +1,25 @@
-% Curve fitting script to find best fitting polynomial for a datafile
+% Curve fitting script to find best fitting polynomial for a given datafile
 
-
+% Initialize imported file and filename
 fileName = input('Enter the filename within single apostrophes as [FILENAME.ext]');
-file = importdata(fileName)
+file = importdata(fileName);
 
+% Parse imported file for data labels and values
 cheaders = file.colheaders;
 data = file.data;
 x = data(:,1);
 y = data(:,2);
 
+% Find line of best fit for data values up to a fourth-order polynomial
 for i = 1:4
     [p, S] = polyfit(x, y, i);
-    rSquaredCompare(i) = S.rsquared;
+    r = 1 - (S.normr/norm(y-mean(y)))^2;
+    rSquaredCompare(i) = r;
 end
 
-[M, r] = max(rSquaredCompare);
-plot(x,y,'o', x, polyval(polyfit(x,y,r),x))
-legend('Imported Values','Line of Best Fit')
+% Find the best fitting polynomial function and plot it
+[M, j] = max(rSquaredCompare);
+plot(x,y,'o', x, polyval(polyfit(x,y,j),x))
+legend('Imported Values', 'Polynomial line of best fit')
 xlabel(cheaders(1))
 ylabel(cheaders(2))
