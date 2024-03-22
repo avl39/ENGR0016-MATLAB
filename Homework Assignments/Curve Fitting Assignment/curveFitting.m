82be6c10edac81dccea926f6c4b94c281756a0eb
avl39 <avl39@pitt.edu>
2024-03-22 16:01:28 -0400

    Added comments to curveFitting.m

--- Homework Assignments/Curve Fitting Assignment/curveFitting.m
+++ Homework Assignments/Curve Fitting Assignment/curveFitting.m
@@ -13,13 +13,13 @@ y = data(:,2);
 % Find line of best fit for data values up to a fourth-order polynomial
 for i = 1:4
     [p, S] = polyfit(x, y, i);
-    r = 1 - (S.normr/norm(y-mean(y)))^2;    % Calculate rsquared values for each polynomial
-    rSquaredCompare(i) = r;                 % Store rsquared values in a vector called rSquaredCompare
+    r = 1 - (S.normr/norm(y-mean(y)))^2;    % Calculate R^2 values for each polynomial
+    rSquaredCompare(i) = r;                 % Store R^2 values in a vector called rSquaredCompare
 end
 
 % Find the best fitting polynomial function and plot it
-[M, j] = max(rSquaredCompare);
-plot(x,y,'o', x, polyval(polyfit(x,y,j),x))
+[M, j] = max(rSquaredCompare); % Pull out the location(degree) of the polynomial with the best R^2 value
+plot(x,y,'o', x, polyval(polyfit(x,y,j),x)) % Plot the imported data against the line of best fit
 legend('Imported Values', 'Polynomial line of best fit')
 xlabel(cheaders(1))
 ylabel(cheaders(2))
